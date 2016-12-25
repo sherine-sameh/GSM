52113af93e04fabf86ddc7bf45df299c1a3d964c
hackerghost93 <ahmedws93@hotmail.com>
2016-12-25 07:50:23 +0200

    Change Analog to digital converter implementation.

--- ADC.m
+++ ADC.m
@@ -1,36 +1,25 @@
-function code = AnalogToDigital (filename)
-load filename ;
-n =1 ;
-m = 8000;
-fs=8000;
-[M,Fs] = audioread(filename);
-code= zeros(1 , m ) ;
-for i= n : m 
-samples = [n,m] ;
-[y,Fs] = audioread(filename,samples);
-sound(y,Fs)
-plot(y);
-Fs
-%partition=[ -0.08 -0.04  -0.02  0 0.02 0.04 0.08 ];
-%codebook=[-0.08 -0.04 -0.02  0 0.02 0.04 0.08 1 ];
-min(y);
-max(y);
-if  and(max(y),min(y)) == 0  
-num= 8192/2 ;
-partition=  -num : 1 : num ;
-codebook =  -num : 1 : num+1 ;
-else avg=abs(min(y)+max(y))/2 
-num = (8192*avg)/2
-partition=  -num : avg : num ;
-codebook =  -num : avg : num+avg ;
-end
-[index,quants] =quantiz(y,partition,codebook);
-%figure 
-%stem(quants);
-code = dec2bin(index);
-n=m
-m = m+fs 
-if m> length(M)
-m = length(M)
-end
-end
+function [binary_signal,dynamic_range] = ADC(filename)
+    fs=8000;
+    [digital_audio_pure,Fs] = audioread(filename);
+    figure;
+    subplot(3,1,1);
+    time = 0:(length(digital_audio_pure)-1); 
+    time = time / Fs ;
+    disp('length of array before sampling');
+    disp(length(time));
+    plot(time,digital_audio_pure);
+    digital_audio_sampled = resample(digital_audio_pure,fs,Fs); % resample
+    subplot(3,1,2);
+    time = 0:(length(digital_audio_sampled)-1);
+    time = time / fs ;
+    plot(time,digital_audio_sampled);
+    disp('length of array after sampling');
+    disp(length(time));
+    disp('rational signal is ');
+    disp(fs/Fs);
+    dynamic_range = 1.2; % calculated from minimum to maximum
+    encoded_signal = uencode(digital_audio_sampled, 13 , dynamic_range);
+    subplot(3,1,3);
+    plot(time,encoded_signal,'.');
+    binary_signal = de2bi(encoded_signal,13);
+ end
